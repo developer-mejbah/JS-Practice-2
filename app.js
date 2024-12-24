80da8fd402b14685bd0cac93b9218f07dd9eefac
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-25 04:20:27 +0600

    Function JS Done

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 
-// let num1 = prompt('Type Your First Number');
-// num1 = parseInt(num1);
+let num1 = prompt('Type Your First Number');
+num1 = parseInt(num1);
 
-// let num2 = prompt('Type Your Second Number')
-// num2 = parseInt(num2);
+let num2 = prompt('Type Your Second Number')
+num2 = parseInt(num2);
 
-// let num3 = prompt('Type Your Third Number')
-// num3 = parseInt(num3);
+let num3 = prompt('Type Your Third Number')
+num3 = parseInt(num3);
 
-// console.log(num1, num2, num3);
+console.log(num1, num2, num3);
 
-// if (num1 > num2 && num1 > num3) {
-//     console.log('Large Number 1');
-// } 
-// else if (num2 > num1 && num2 > num3){
-//     console.log('Large Number 2');
-// }
-// else if (num3 > num1 && num3 > num1){
-//     console.log('Large Number 3');
-// } else {
-//     console.log('Invalid Number');
-// }
+if (num1 > num2 && num1 > num3) {
+    console.log('Large Number 1');
+} 
+else if (num2 > num1 && num2 > num3){
+    console.log('Large Number 2');
+}
+else if (num3 > num1 && num3 > num1){
+    console.log('Large Number 3');
+} else {
+    console.log('Invalid Number');
+}
 
 // Array
 
-// const countries = ['Bangladesh', 'India', 'Pakistan', 'Srilanka', 'Maldives', 'United States'];
-// console.log(countries);
-// for (let k = 0; k <= countries.length - 1; k++){
-//     console.log(k)
-//     console.log(countries[k])
-// }
-// countries [3] = 'China';
-// countries [1] = 'Srilanka';
-// countries.push('Russia')
-// countries.push('Saudi Arabia')
-// countries.push('Japan')
-// countries.push('Mongolia')
-// countries.push('Australia')
-// countries.push('India')
-// countries.pop()
-// console.log(countries);
+const countries = ['Bangladesh', 'India', 'Pakistan', 'Srilanka', 'Maldives', 'United States'];
+console.log(countries);
+for (let k = 0; k <= countries.length - 1; k++){
+    console.log(k)
+    console.log(countries[k])
+}
+countries [3] = 'China';
+countries [1] = 'Srilanka';
+countries.push('Russia')
+countries.push('Saudi Arabia')
+countries.push('Japan')
+countries.push('Mongolia')
+countries.push('Australia')
+countries.push('India')
+countries.pop()
+console.log(countries);
 
-// const fifaRankings = ['1. Argentina', '2.France', '3.Spain', 'England', '5.Brazil'];
-// console.log(fifaRankings);
+const fifaRankings = ['1. Argentina', '2.France', '3.Spain', 'England', '5.Brazil'];
+console.log(fifaRankings);
 
 const ages = [15, 24, 35, 40, 45, 50, 52, 77, 12, 44, 23,];
 const newAgesEven = [];
@@ -84,53 +84,77 @@ for (let n = 0; n < newAgesOdd.length; n++)
 }
 console.log(total2)
 // Most Important
-// console.log(ages[ages.length - 1]);
-// console.log(ages.indexOf(77));
-// console.log(ages.length);
-// console.log(ages [6]);
+console.log(ages[ages.length - 1]);
+console.log(ages.indexOf(77));
+console.log(ages.length);
+console.log(ages [6]);
 
-// const birthYear = 2003 ;
-// const mejbahMorsalin = ['Mejbah Morsalin', 2024 - birthYear];
-// console.log(2024 - birthYear);
+const birthYear = 2003 ;
+const mejbahMorsalin = ['Mejbah Morsalin', 2024 - birthYear];
+console.log(2024 - birthYear);
 
-// const number = [ 200, 650, 400, 300, 45, 56, 12,];
-// console.log(number);
+const number = [ 200, 650, 400, 300, 45, 56, 12,];
+console.log(number);
 
-// number.unshift(100);
-// console.log(number);
+number.unshift(100);
+console.log(number);
 
-// number.shift();
-// console.log(number);
+number.shift();
+console.log(number);
 
 // Loop Started
 // for loop & while loop
 // for (initialization; condition ; update){}
 
 // for loop
-// let sum = 0;
-// for (let i = 1; i <= 15; i++) {
-//     console.log(i);
-//     sum = sum + i;
-// }
-// console.log(sum)
+let sum = 0;
+for (let i = 1; i <= 15; i++) {
+    console.log(i);
+    sum = sum + i;
+}
+console.log(sum)
 
-// let add = 0;
-// for (let i = 1; i <= 20; i = i + 2){
-//     console.log(i);
-//     add = add + i;
-// }
-// console.log(add);
+let add = 0;
+for (let i = 1; i <= 20; i = i + 2){
+    console.log(i);
+    add = add + i;
+}
+console.log(add);
 
-// added = 0;
-// for (let i = 2; i <= 20; i = i + 2){
-//     console.log(i);
-//     added = added + i;
-// }
-// console.log(added);
+added = 0;
+for (let i = 2; i <= 20; i = i + 2){
+    console.log(i);
+    added = added + i;
+}
+console.log(added);
 
 // while loop
-// let h = 0;
-// while (h <= 10){
-//     console.log(h);
-//     h++;
-// }
+let h = 0;
+while (h <= 10){
+    console.log(h);
+    h++;
+}
+
+// Js Function
+// function add(){}
+function calcAge (birthYear2) {
+    let age5 = 2023 - birthYear2;
+    return age5;
+}
+const age2 = calcAge(1992);
+const age3 = calcAge(1996);
+const age4 = calcAge(1999);
+console.log(age2 , age3 , age4);
+
+const rolls = [15, 24, 35, 40, 45, 50, 52, 77, 12, 44, 23,];
+
+function arrSum(rolls){
+    let sum1 =0;
+    for (let roll of rolls){
+        console.log(roll);
+        sum1 = sum1 + roll;
+    }
+    return sum1
+}
+let all = arrSum(rolls)
+console.log(all)
