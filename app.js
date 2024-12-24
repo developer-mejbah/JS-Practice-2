0b6905eb2388265381572cd365204b672b4dfc3d
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-25 01:24:25 +0600

    Comment Cleard

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 
-// let num1 = prompt('Type Your First Number');
-// num1 = parseInt(num1);
+let num1 = prompt('Type Your First Number');
+num1 = parseInt(num1);
 
-// let num2 = prompt('Type Your Second Number')
-// num2 = parseInt(num2);
+let num2 = prompt('Type Your Second Number')
+num2 = parseInt(num2);
 
-// let num3 = prompt('Type Your Third Number')
-// num3 = parseInt(num3);
+let num3 = prompt('Type Your Third Number')
+num3 = parseInt(num3);
 
-// console.log(num1, num2, num3);
+console.log(num1, num2, num3);
 
-// if (num1 > num2 && num1 > num3) {
-//     console.log('Large Number 1');
-// } 
-// else if (num2 > num1 && num2 > num3){
-//     console.log('Large Number 2');
-// }
-// else if (num3 > num1 && num3 > num1){
-//     console.log('Large Number 3');
-// } else {
-//     console.log('Invalid Number');
-// }
+if (num1 > num2 && num1 > num3) {
+    console.log('Large Number 1');
+} 
+else if (num2 > num1 && num2 > num3){
+    console.log('Large Number 2');
+}
+else if (num3 > num1 && num3 > num1){
+    console.log('Large Number 3');
+} else {
+    console.log('Invalid Number');
+}
 
 // Array
 
